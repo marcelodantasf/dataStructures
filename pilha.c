c301d9beddf0a682c90bb9e8663fe33d6e07436f
marcelodantasf <mdantasfilho2005@gmail.com>
2024-08-07 12:00:15 -0300

    feat: added stack file

---
+++ pilha.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int capacidade;
+    int topo;
+    int *dados;
+} pilha;
+
+void init(pilha *p, int cap){
+    p->capacidade = cap;
+    p->topo = -1;
+    p->dados = calloc(p->capacidade,sizeof(int));
+    printf("Memory space allocated successfully!\n");
+}
+
+void list(pilha *p) {
+    printf("Queue:\n");
+    for(int i = 0; i < p->topo+1; i++) {
+        printf("[%d] ",*(p->dados+i));
+    }
+    printf("\n");
+}
+
+void free_all(pilha *p){
+    free(p->dados);
+    printf("Memory freed!\n");
+}
+
+int insert(pilha *p, int valor){
+    if (p->topo + 1 == p->capacidade){
+        printf("ERROR!\nNo space available\n");
+        return -1;
+    }
+    int i = 0;
+    int *aux;
+    aux = calloc(p->capacidade, sizeof(int));
+    // copiar todo o conteudo de *dados em *aux
+    while(i < p->topo + 1) {
+        *(aux + i) = *(p->dados + i);
+        i++;
+    }
+    i = 0;
+    *p->dados = valor;
+    while(i < p->topo + 1){
+        *(p->dados+i+1) = *(aux+i);
+        i++;
+    }
+    printf("Value [%d] added to stack!\n", valor);
+    p->topo += 1;
+}
+
+int delete(pilha *p){
+    if (p->topo < 0)
+    {
+        printf("ERROR!\nEmpty stack!\n");
+        return -1;
+    }
+    p->topo -= 1;
+    printf("Element removed from queue!\n");
+}
+
+int main(){
+    pilha stack;
+    init(&stack,5);
+    insert(&stack, 10);
+    insert(&stack, 8);
+    insert(&stack, 6);
+    list(&stack);
+    delete(&stack);
+    list(&stack);
+    exit(0);
+}
