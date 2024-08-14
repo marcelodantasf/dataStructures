7fe647a261546db391604efe763d533e92041906
marcelodantasf <mdantasfilho2005@gmail.com>
2024-08-13 23:20:29 -0300

    refactor: finished changes + testing

--- nodeStructures/pilhaNo.c
+++ nodeStructures/pilhaNo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     int id;
@@ -13,6 +14,7 @@ typedef struct {
 
 void push(no** p, dados data) {
     if(*p == NULL){
+        *p = malloc(sizeof(no));
         (*p)->d = data;
         (*p)->prox = NULL;
 
@@ -67,9 +69,40 @@ void list(no* p) {
 
     no* getter = p;
 
+    printf("Reading:\n");
     while(getter){
-        printf("[%d] ", p->d.id);
+        printf("[id: %d] [name: %s]\n", getter->d.id, getter->d.name);
         getter = getter->prox;
     }
     printf("\n");
+}
+
+dados setData(dados d, int userId, char userName[]) {
+    d.id = userId;
+    strcpy(d.name,userName);
+
+    return d;
+}
+
+int main(){
+    dados e1 = setData(e1,1,"angela");
+    dados e2 = setData(e2,2,"camille");
+    dados e3 = setData(e3,3,"jack");
+
+    no* stack = NULL;
+
+    push(&stack, e1);
+    push(&stack, e2);
+
+    list(stack);
+
+    push(&stack, e3);
+
+    list(stack);
+
+    pop(&stack);
+
+    list(stack);
+
+    return 0;
 }
